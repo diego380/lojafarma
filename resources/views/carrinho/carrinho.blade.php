cac825441037aae28eb84772ca5aac0add61d6fd
Diego Melo <diegomelo.90@hotmail.com>
2017-10-09 11:47:47 -0300

    carrinho de compras ajustado

--- resources/views/carrinho/carrinho.blade.php
+++ resources/views/carrinho/carrinho.blade.php
@@ -4,42 +4,74 @@
 
 @section('content')
 
-<div class="conteiner">
+<div class="container">
 	<div class="row">
-		<div class="col-md-6">
-			<h2>
-				<span class="glyphicon glyphicon-shopping-cart"></span> Carrinho de compras 
-			</h2>
+		<div class="col-xs-10">
+			<div class="panel panel-info">
+				<div class="panel-heading">
+					<div class="panel-title">
+						<div class="row">
+							<div class="col-xs-6">
+								<h5><span class="glyphicon glyphicon-shopping-cart"></span> Carrinho de compras</h5>
+							</div>
+							<div class="col-xs-6">
+								<button type="button" class="btn btn-primary btn-sm pull-right">
+									<span class="glyphicon glyphicon-share-alt"></span> Continuar comprando
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-xs-2"><img class="img-responsive" src="{{ url('/img/tarjas.png') }}">
+						</div>
+						<div class="col-xs-4">
+							<h4 class="product-name"><strong>ABRIFIT XPE C/100ML HEDERA+COPO DOSADOR </strong></h4>
+						</div>
+						<div class="col-xs-6">
+							<div class="col-xs-6 text-right">
+								<h6><strong>R$ 25.00 <span class="text-muted">x</span></strong></h6>
+							</div>
+							<div class="col-xs-4">
+								<input type="text" class="form-control input-sm" value="1">
+							</div>
+							<div class="col-xs-2">
+								<button type="button" class="btn btn-link btn-xs">
+									<span class="glyphicon glyphicon-trash"> </span>
+								</button>
+							</div>
+						</div>
+					</div>
+					<hr>
+					<div class="row">
+						<div class="text-center">
+							<div class="col-xs-9">
+								<h6 class="text-right">Adicionou item(s)?</h6>
+							</div>
+							<div class="col-xs-3">
+								<button type="button" class="btn btn-default btn-sm btn-block">
+									Atualizar carrinho de compras
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="panel-footer">
+					<div class="row text-center">
+						<div class="col-xs-9">
+							<h4 class="text-right">Total <strong>R$ 25.00</strong></h4>
+						</div>
+						<div class="col-xs-3">
+							<button type="button" class="btn btn-success btn-block">
+								Checkout
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
-	<div>
-		<div class="col-md-12">
-			<table class="table table-striped">
-				<th>
-					<td>Produto</td>
-					<td>Quantidade</td>
-					<td>Preço</td>
-					<td>Total</td>
-					<td>Ação</td>
-				</th>
-				<tr>
-					<td>data</td>
-					<td>data</td>
-					<td class="col-sm-1 col-md-1">
-						<input class="form-control" type="text" name="" value="" placeholder="">
-					</td>
-					<td>data</td>
-					<td>data</td>
-					<td>
-						<button class="btn btn-danger">
-							<span class="glyphicon glyphicon-remove"></span>
-						</button>
-					</td>
-				</tr>
-			</table>
-		</div>
-	</div>
-	
 </div>
 
 
